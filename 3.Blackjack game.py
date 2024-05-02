d96f729e54d2ec12254b402450e4cc3c25fcbb28
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2024-05-02 22:59:04 +0530

    Update 3.Blackjack game.py

--- 3.Blackjack game.py
+++ 3.Blackjack game.py
@@ -3,7 +3,6 @@ import random, sys
 HEARTS   = chr(9829) # Character 9829 is '♥'.
 DIAMONDS = chr(9830) # Character 9830 is '♦'.
 SPADES   = chr(9824) # Character 9824 is '♠'.
-CLUBS    = chr(9827) # Character 9827 is '♣'.
 BACKSIDE = 'backside'
 
 
